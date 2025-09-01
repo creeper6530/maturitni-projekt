57a34d3049219c7c6f223e66a04d8e2603ebb541
creeper6530 <o.behal@centrum.cz>
2025-09-01 13:58:08 +0200

    Quickly got up to speed, now displaying debug data

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,55 @@
 #![no_std]
 #![no_main]
 
+const I2C_FREQ_KHZ: u32 = 1000; // 1 MHz, the maximum speed for I²C on the RP2040 (so-called Fast Mode Plus; datasheet 4.3.3), and the SSD1306 can handle it well
+
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::OutputPin;
 use panic_probe as _;
 
 use rp2040_hal as hal;
-use rp2040_hal::pac as pac;
-
 use hal::{
+    pac,
+
     clocks::{Clock, init_clocks_and_plls},
-    sio::Sio,
     watchdog::Watchdog,
+    
+    sio::Sio,
 };
+use rp2040_hal::fugit::RateExtU32; // For the `.kHz()` method on u32 integers
+use cortex_m::asm;
+
+// Display imports
+use embedded_graphics::{prelude::*, image::Image};
+use ssd1306::{prelude::*, Ssd1306};
+use tinybmp::Bmp;
+
+use core::cell::RefCell;
+//use core::ops::DerefMut;
+
+mod stack;
+use stack::*;
+mod textbox;
+use textbox::*;
 
 #[unsafe(link_section = ".boot2")]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+
+defmt::timestamp!("{=u64:us}", {
+    /* Stolen from `https://docs.rs/rp2040-hal/latest/src/rp2040_hal/timer.rs.html#69-88`
+    and `https://defmt.ferrous-systems.com/timestamps`, though customised greatly.
+    We use the critical section to ensure no disruptions, because reading L latches the H register (datasheet section 4.6.2)
+    It could have unforseen consequences if we try reading again while there's already a read in progress. */
+
+    // Safety: We are guaranteed that the PTR points to a valid place, since we assume the `pac` is infallible.
+    let timer_regs = unsafe { &*pac::TIMER::PTR }; // We dereference the TIMER peripheral's raw pointer and get a normal reference to it.
+    critical_section::with(|_| {
+        let low: u32 = timer_regs.timelr().read().bits();
+        let hi: u32 = timer_regs.timehr().read().bits();
+        ((hi as u64) << 32) | (low as u64)
+    })
+});
 
 #[hal::entry]
 fn main() -> ! {
@@ -38,11 +70,9 @@ fn main() -> ! {
         pac.PLL_USB,
         &mut pac.RESETS,
         &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
+    ).unwrap();
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    trace!("Clocks initialized");
 
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
@@ -51,15 +81,63 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut led_pin = pins.gpio25.into_push_pull_output();
+    let i2c = hal::I2C::i2c0(
+        pac.I2C0,
+        pins.gpio8.reconfigure(),
+        pins.gpio9.reconfigure(),
+        I2C_FREQ_KHZ.kHz(),
+        &mut pac.RESETS,
+        &clocks.peripheral_clock,
+    );
+    trace!("I²C initialized");
+
+    let iface = ssd1306::I2CDisplayInterface::new(i2c);
+    let mut disp = Ssd1306::new(iface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    disp.init().unwrap();
+    disp.set_brightness(Brightness::BRIGHTEST).unwrap(); // TODO: Set the brightness with a potentiometer (probably poll, not ADC interrupt due to noise)
+
+    // We show a Rust logo bitmap on the display as a loading screen
+    // We're showing it as soon as possible once the display and everything it needs is initialized
+    Image::new(
+        &Bmp::from_slice(include_bytes!("rust.bmp")).unwrap(),
+        (32, 0).into(), // The image is 64x64, so we center it horizontally
+    )
+    .draw(&mut disp).unwrap();
+    disp.flush().unwrap();
+    trace!("Display initialized");
+
+    // Let me ask one question: Why the hell can't this be as straightforward as I²C is?
+    let uart = hal::uart::UartPeripheral::new(
+        pac.UART0,
+        (pins.gpio0.into_function(), pins.gpio1.into_function()), // Luckily the function itself is inferred, so we don't need to specify it explicitly
+        &mut pac.RESETS
+    )
+    .enable(hal::uart::UartConfig::default(), clocks.peripheral_clock.freq()) // Default is a sane 115200 8N1
+    .unwrap();
+    trace!("UART initialized");
+
+    let disp_refcell = RefCell::new(disp);
+    // Range of isize is `-2147483648..=2147483647`
+    let mut stack: CustomStack<'_, isize, _, _> = CustomStackBuilder::<'_, isize, _, _>::new(&disp_refcell) // We're using the turbofish syntax here
+        .build();
+    let mut textbox: _ = CustomTextboxBuilder::new(&disp_refcell)
+        .build();
+
+    stack.push(5).unwrap();
+    stack.push(6).unwrap();
+    stack.push(7).unwrap();
+    stack.push(8).unwrap();
+    stack.push(9).unwrap();
+    stack.push(10).unwrap();
+    textbox.append_str("DEBUG TEXTBOX DEBUG!").unwrap();
+
+    delay.delay_ms(2_000);
+    stack.draw(false);
+    textbox.draw(true);
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        asm::wfi();
     }
 }
 
---
+++ src/textbox.rs
@@ -0,0 +1,327 @@
+// Imports for stuff to work
+#![allow(unused_imports)]
+use embedded_graphics::{
+    prelude::*,
+    pixelcolor::BinaryColor,
+
+    mono_font::{
+//        ascii::FONT_6X12,
+        iso_8859_2::FONT_6X12 as ISO_FONT_6X12,
+        MonoTextStyle,
+        MonoTextStyleBuilder
+    },
+    text::{
+        Baseline,
+        Text,
+    },
+
+    primitives::{
+        PrimitiveStyle,
+        PrimitiveStyleBuilder,
+        Rectangle,
+    },
+};
+use ssd1306::{
+    Ssd1306,
+    prelude::*,
+    mode::BufferedGraphicsMode,
+};
+
+// Imports for the actual code
+use heapless::{Vec, String};
+use core::{
+    prelude::v1::*, // I sincerely hope this is unnecessary, but who knows?
+    cell::RefCell, // For the `RefCell` type
+    cmp::min, // For the `min` function
+    ops::DerefMut, // For the `deref_mut` method
+    fmt::Write, // For the `write!` macro
+};
+
+// Debugging imports
+use defmt::*;
+use defmt_rtt as _;
+use panic_probe as _;
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Note: these constants are copied in `stack.rs` as well, maintain consistency between the two files!
+// TODO: Make a common file for constants? Could even encompass `main.rs`.
+
+// Compile time constants
+/** The fonts we use usually have unused pixels at the top that'd waste space,
+so with this constant we basically cut off the top `n` pixels. */
+const PIXELS_REMOVED: u8 = 2;
+/// Size of String-s used for buffering text during writes, and for the textbox
+const TEXT_BUFFER_SIZE: usize = 32;
+/** Number of pixels to offset the textbox from the bottom of the display by
+
+This constant shall be determined by the programmer,
+as we won't know the font size at compile time,
+and going off of defaults beats the point of the ability to change the defaults. */
+const TEXTBOX_OFFSET: u8 = 4;
+/** Whether to draw a cursor under the text of the textbox
+
+May only be true of we give it the space with the TEXTBOX_OFFSET const
+-- if the const is larger than one */
+const TEXTBOX_CURSOR: bool = true;
+
+// Evaluated at compile time to ensure that the constants are valid
+const fn _check_consts() {
+    if TEXTBOX_CURSOR && TEXTBOX_OFFSET < 1 {
+        core::panic!("TEXTBOX_CURSOR can only be true if TEXTBOX_OFFSET is larger than 1");
+    }
+}
+const _: () = _check_consts();
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct DisplayDimensions {
+    pub width: u8,
+    pub height: u8,
+}
+
+impl From<(u8, u8)> for DisplayDimensions {
+    fn from(dimensions: (u8, u8)) -> Self {
+        return DisplayDimensions {
+            width: dimensions.0,
+            height: dimensions.1,
+        };
+    }
+}
+
+impl Default for DisplayDimensions {
+    fn default() -> Self {
+        return DisplayDimensions {
+            width: 128,
+            height: 64,
+        };
+    }
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+pub struct CustomTextboxBuilder<'a, DI, SIZE>
+where
+    DI: WriteOnlyDataCommand,
+    SIZE: DisplaySize,
+{
+    text: String<TEXT_BUFFER_SIZE>,
+
+    display_refcell: &'a RefCell<Ssd1306<DI, SIZE, BufferedGraphicsMode<SIZE>>>,
+    disp_dimensions: DisplayDimensions,
+
+    text_style: MonoTextStyle<'a, BinaryColor>,
+    primitives_style: PrimitiveStyle<BinaryColor>,
+    primitives_alternate_style: PrimitiveStyle<BinaryColor>,
+}
+
+#[allow(dead_code)]
+impl<'a, DI, SIZE> CustomTextboxBuilder<'a, DI, SIZE>
+where 
+    DI: WriteOnlyDataCommand,
+    SIZE: DisplaySize,
+{
+    /// Creates a new `CustomTextboxBuilder` with the given display RefCell.
+    /// 
+    /// This constructor uses the default display dimensions of 128x64 pixels and the default text style.
+    /// For custom parameters, use [`Self::new_custom()`].
+    pub fn new(
+        display_refcell: &'a RefCell<Ssd1306<DI, SIZE, BufferedGraphicsMode<SIZE>>>
+    ) -> Self {
+        return CustomTextboxBuilder {
+            text: String::new(),
+            
+            disp_dimensions: DisplayDimensions::default(),
+            display_refcell,
+
+            // Standard white text on transparent background
+            text_style: MonoTextStyleBuilder::new()
+                .font(&ISO_FONT_6X12)
+                .text_color(BinaryColor::On)
+                //.reset_background_color() // Reset the background color to transparent (unnecessary, but for clarity)
+                .build(),
+
+            // Standard white stroke with 1px width and transparent fill
+            primitives_style: PrimitiveStyleBuilder::new()
+                .stroke_width(1)
+                .stroke_color(BinaryColor::On)
+                //.reset_fill_color() // Reset the fill color to transparent (unnecessary, but for clarity)
+                .build(),
+
+            // Standard black stroke with 1px width and black fill
+            primitives_alternate_style: PrimitiveStyleBuilder::new()
+                .stroke_width(1)
+                .stroke_color(BinaryColor::Off)
+                .fill_color(BinaryColor::Off)
+                .build(),
+        };
+    }
+
+    pub fn build(self) -> CustomTextbox<'a, DI, SIZE> {
+        return CustomTextbox {
+            text: self.text,
+
+            disp_dimensions: self.disp_dimensions,
+            display_refcell: self.display_refcell,
+
+            text_style: self.text_style,
+            primitives_style: self.primitives_style,
+            primitives_alternate_style: self.primitives_alternate_style,
+
+            debug: false,
+        };
+    }
+
+    pub fn build_debug(mut self) -> CustomTextbox<'a, DI, SIZE> {
+        warn!("Building a debug textbox, filling it with default values.");
+
+        self.text.clear();
+        self.text.push_str("DEBUG TEXTBOX").unwrap();
+
+        return CustomTextbox {
+            // Fill the text with a debug message
+            text: self.text,
+
+            disp_dimensions: self.disp_dimensions,
+            display_refcell: self.display_refcell,
+
+            text_style: self.text_style,
+            primitives_style: self.primitives_style,
+            primitives_alternate_style: self.primitives_alternate_style,
+
+            debug: true,
+        };
+    }
+
+    pub fn set_disp_dimensions(mut self, dimensions: DisplayDimensions) -> Self {
+        self.disp_dimensions = dimensions;
+        return self;
+    }
+
+    pub fn set_text_style(mut self, text_style: MonoTextStyle<'a, BinaryColor>) -> Self {
+        self.text_style = text_style;
+        return self;
+    }
+
+    pub fn set_primitives_style(mut self, primitives_style: PrimitiveStyle<BinaryColor>) -> Self {
+        self.primitives_style = primitives_style;
+        return self;
+    }
+
+    pub fn set_primitives_alternate_style(mut self, primitives_alternate_style: PrimitiveStyle<BinaryColor>) -> Self {
+        self.primitives_alternate_style = primitives_alternate_style;
+        return self;
+    }
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+#[allow(dead_code)]
+pub struct CustomTextbox<'a, DI, SIZE>
+where
+    DI: WriteOnlyDataCommand,
+    SIZE: DisplaySize,
+{
+    text: String<TEXT_BUFFER_SIZE>,
+
+    disp_dimensions: DisplayDimensions,
+    display_refcell: &'a RefCell<Ssd1306<DI, SIZE, BufferedGraphicsMode<SIZE>>>,
+
+    text_style: MonoTextStyle<'a, BinaryColor>,
+    primitives_style: PrimitiveStyle<BinaryColor>,
+    primitives_alternate_style: PrimitiveStyle<BinaryColor>,
+
+    debug: bool,
+}
+
+#[allow(dead_code)]
+impl<'a, DI, SIZE> CustomTextbox<'a, DI, SIZE>
+where
+    DI: WriteOnlyDataCommand,
+    SIZE: DisplaySize,
+{
+    pub fn draw(&self, flush: bool) {
+
+        let mut display_refmut = self.display_refcell.borrow_mut();
+        let display_ref = display_refmut.deref_mut();
+
+        let text_height = self.text_style.font.character_size.height as u8 - PIXELS_REMOVED;
+        let textbox_height = text_height + TEXTBOX_OFFSET; // The height of the whole textbox is the height of one line of text plus the offset
+
+        Rectangle::with_corners(
+            (0, self.disp_dimensions.height as i32 - 1).into(), // Bottom right corner
+            (
+                self.disp_dimensions.width as i32 - 1,
+                (self.disp_dimensions.height - textbox_height) as i32
+            ).into() // Top left corner
+        )
+        .into_styled(
+            if self.debug {
+                self.primitives_style // If we're in debug mode, we use the normal style to draw white boundaries
+            } else {
+                self.primitives_alternate_style // Otherwise, we use the alternate style to draw a black rectangle - clearing the area
+            }
+        )
+        .draw(display_ref)
+        .unwrap();
+
+        Text::with_baseline(
+            self.text.as_str(),
+            (0, (self.disp_dimensions.height - textbox_height) as i32).into(), // Top left corner
+            self.text_style,
+            Baseline::Top
+        )
+        .draw(display_ref)
+        .unwrap();
+
+        if TEXTBOX_CURSOR {
+            let cursor_height = TEXTBOX_OFFSET - 1;
+
+            // Draw the cursor under the text
+            Rectangle::new(
+                (
+                    self.text.chars().count() as i32 * self.text_style.font.character_size.width as i32 + 1, 
+                    (self.disp_dimensions.height - 1 - cursor_height) as i32
+                ).into(),
+                (self.text_style.font.character_size.width, cursor_height as u32).into()
+            )
+            .into_styled(self.primitives_style)
+            .draw(display_ref)
+            .unwrap();
+        }
+
+        if flush { display_ref.flush().unwrap(); };
+    }
+
+    pub fn append_str(&mut self, string: &str) -> Result<(), ()> {
+        if self.text.len() + string.len() > TEXT_BUFFER_SIZE {
+            warn!("Tried to append a string that is too long for the textbox, returning Err.");
+            return Err(());
+        }
+
+        return self.text.push_str(string).map_err(|_| ()); // `heapless` v0.9 changed the error type of `push` and `push_str` from `()` to `CapacityError`, we map it back to `()`
+    }
+
+    pub fn append_char(&mut self, c: char) -> Result<(), ()> {
+        if self.text.len() + c.len_utf8() > TEXT_BUFFER_SIZE { // We care about the UTF-8 byte length, not the number of characters
+            warn!("Tried to append a character that is too long for the textbox, returning Err.");
+            return Err(());
+        }
+
+        return self.text.push(c).map_err(|_| ());
+    }
+
+    pub fn get_text(&self) -> String<TEXT_BUFFER_SIZE> {
+        return self.text.clone();
+    }
+
+    pub fn clear(&mut self) {
+        warn!("Clearing the textbox, all text will be lost.");
+        self.text.clear();
+    }
+
+    pub fn len(&self) -> usize {
+        return self.text.len();
+    }
+}
